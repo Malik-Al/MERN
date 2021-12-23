193dfce858bd4bd86807f11b34e3dd0d4af0dd72
malik <Malik-Al>
2021-12-23 23:01:28 +0600

    Добавлено различное представление файлов

--- client/src/components/disk/fileList/FileList.jsx
+++ client/src/components/disk/fileList/FileList.jsx
@@ -6,6 +6,7 @@ import {CSSTransition, TransitionGroup} from "react-transition-group";
 
 const FileList = () => {
     const files = useSelector(state => state.files.files)
+    const fileView = useSelector(state => state.files.view)
 
     if(files.length === 0){
         return (
@@ -13,27 +14,41 @@ const FileList = () => {
         )
     }
 
-    return (
-        <div className='fileList'>
-            <div className="fileList__header">
-                <div className="fileList__name">Название</div>
-                <div className="fileList__data">Дата</div>
-                <div className="fileList__size">Размер</div>
-            </div>
-            <TransitionGroup>
+    if(fileView === 'plate'){
+        return (
+            <div className='filePlate'>
                 {files.map((file, id) =>
-                    <CSSTransition
-                        key={id}
-                        timeout={500}
-                        classNames={'file'}
-                        exit={false}
-                    >
-                        <File file={file} />
-                    </CSSTransition>
+                     <File key={id} file={file}/>
                 )}
-            </TransitionGroup>
-       </div>
-    );
+            </div>
+        )
+    }
+
+    if(fileView === 'list'){
+        return (
+            <div className='fileList'>
+                <div className="fileList__header">
+                    <div className="fileList__name">Название</div>
+                    <div className="fileList__data">Дата</div>
+                    <div className="fileList__size">Размер</div>
+                </div>
+                <TransitionGroup>
+                    {files.map((file, id) =>
+                        <CSSTransition
+                            key={id}
+                            timeout={500}
+                            classNames={'file'}
+                            exit={false}
+                        >
+                            <File file={file} />
+                        </CSSTransition>
+                    )}
+                </TransitionGroup>
+            </div>
+        );
+    }
+
+
 };
 
 export default FileList;
